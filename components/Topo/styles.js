c957dcb653da9663d4812c5659cbf77983910e26
Matheus Folitti <matheusfoliatti@gmail.com>
2023-04-16 17:49:31 -0300

    samueljacksonfive

---
+++ components/Topo/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const stylesTopo = StyleSheet.create({
+    containerTopo: {
+        backgroundColor:'darkblue',
+        width: '100%',
+        padding:'20px'
+    },
+    title: {
+        color:'white',
+        fontWeight:'bold',
+        fontSize:'20px',
+    }
+})
+
+export default stylesTopo;
